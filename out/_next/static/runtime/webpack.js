7690c6209b6cc7d7d1e5d98791eee47e531accba
the-other-ryan-sheehan <ryansheehansotheremail@gmail.com>
2020-12-28 21:20:02 -0500

    svg made a little smaller

--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0a42c382c8cae1ad94d4";
+/******/ 	var hotCurrentHash = "d7b541f25d839d9697e1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
